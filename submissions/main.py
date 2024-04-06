30e745e06ae4a234594009a335ec3b9efb6ff5b9
rahcode7 <rahul008mehta@gmail.com>
2024-04-06 22:26:36 +0530

    codes

--- submissions/main.py
+++ submissions/main.py
@@ -14,7 +14,7 @@ import os
 import easyocr
 from langdetect import detect
 from transformers import M2M100ForConditionalGeneration, M2M100Tokenizer
-
+import shutil
 def get_meme_text(filepath,model,tokenizer,translate=False):
     result = reader.readtext(filepath)
 
@@ -44,7 +44,6 @@ class ClassificationHead(nn.Module):
     
     def forward(self, x):
         return self.fc(x)
-
 
 def classifier(model_dict,checkpoint_file,device,image,text):
     tokenizer = model_dict['tokenizer']
@@ -85,7 +84,7 @@ def classifier(model_dict,checkpoint_file,device,image,text):
     #return "",""
 
 def process_line_by_line(checkpoint_file,reader,device,model_dict,image_path):
-    #ic("Processing image")
+    print("Processing image")
     image = Image.open(image_path).convert('RGB')  
     
     text = get_meme_text(reader,image_path,translation_model,tokenizer,translate=True) # Easyocr
@@ -95,16 +94,14 @@ def process_line_by_line(checkpoint_file,reader,device,model_dict,image_path):
 
 if __name__ == "__main__":
 
-    os.environ['TRANSFORMERS_CACHE'] = '.\.cache'
+    os.environ['TRANSFORMERS_CACHE'] = 'cache'
 
-    #reader = easyocr.Reader(['en'])
-    reader = ""
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    checkpoint_file="checkpoints/checkpoints-flava-ddp-base-29Mar/checkpoint_02.pth"
+    checkpoint_file="checkpoints/checkpoint_05.pth"
 
     # Load model artifacts 
 
-    PATH='.cache/models--facebook--flava-full'
+    PATH='cache/models--facebook--flava-full'
     tokenizer = AutoTokenizer.from_pretrained(PATH, local_files_only=True)
     model = FlavaModel.from_pretrained(PATH, local_files_only=True)
     processor = AutoProcessor.from_pretrained(PATH,local_files_only=True)
@@ -115,19 +112,25 @@ if __name__ == "__main__":
     model_dict = {'tokenizer':tokenizer,'model':model,'processor':processor}
 
     # Text reader
-    reader = easyocr.Reader(['ch_sim','en'],gpu=False) #,'ta','ms','hi','id'])
+    # print(os.getcwd())
+    # print(os.listdir())
+    # #shutil.chown('home/admin/.EasyOCR', user='admin', group=None)
+    # shutil.chown('/home/admin/app/.EasyOCR', user='admin', group=None)
 
+    EASYOCR_PATH="cache" 
+    reader = easyocr.Reader(['ch_sim','en'],model_storage_directory=EASYOCR_PATH,download_enabled=False,user_network_directory=EASYOCR_PATH,gpu=False) #,quantize=False) #,'ta','ms','hi','id'])
+    
     # Translation model load
-    TRANSLATION_MODEL='.cache/models--facebook--m2m100_418M'
+    TRANSLATION_MODEL='cache/models--facebook--m2m100_418M'
     translation_model = M2M100ForConditionalGeneration.from_pretrained(TRANSLATION_MODEL,local_files_only=True)
     tokenizer = M2M100Tokenizer.from_pretrained(TRANSLATION_MODEL,local_files_only=True)
-
+    print("models loaded" )
     #ic(os.getcwd())
     # Iteration loop to get new image filepath from sys.stdin:
     for line in sys.stdin:
         # IMPORTANT: Please ensure any trailing whitespace (eg: \n) is removed. This may impact some modules to open the filepath
         image_path = line.rstrip()
-        #ic(image_path)
+        #print(image_path)
         try:
             # Process the image
             proba, label = process_line_by_line(checkpoint_file,reader,device,model_dict,image_path)
